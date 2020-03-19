a80c99124b9520a36d8c76256be6de6ac74b29da
Zogoo <chtsogbadrakh@gmail.com>
2020-03-19 18:42:01 +0900

    Define accosiation for branch

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@
 class User < ApplicationRecord
   belongs_to :branch
   has_many :accounts
-  has_many :transactions
+  has_many :transactions, through: :accounts
   has_one :profile
 
   enum status: %i[created active suspended blocked deleted]
--- spec/models/branch_spec.rb
+++ spec/models/branch_spec.rb
@@ -10,9 +10,21 @@ RSpec.describe Branch, type: :model do
       let!(:user) { create(:user, branch: atm) }
       let!(:account_from) { create(:account, user: user) }
       let!(:account_to) { create(:account, user: user) }
-      let(:transaction) { create(:transaction, account: account_from, receiver: account_to) }
-      it 'user will associated to branch' do
+      let!(:transaction) { create(:transaction, account: account_from, receiver: account_to) }
+      it 'user will associate to branch' do
         expect(atm.users.count.positive?).to be_truthy
+      end
+
+      it 'accounts blongs to user' do
+        expect(user.accounts.count.positive?).to be_truthy
+      end
+
+      it 'user will have transactions' do
+        expect(user.transactions.count.positive?).to be_truthy
+      end
+
+      it 'transactions will associate to accounts' do
+        expect(account_from.transactions.count.positive?).to be_truthy
       end
     end
   end
